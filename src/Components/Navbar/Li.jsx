a49a2bdd09bd53201322db2fa55ebbf5c1fc35ea
rjspyk5 <rjspyk5@gmail.com>
2024-03-19 15:23:01 +0600

    practising create custom navbar and data fetching axios

---
+++ src/Components/Navbar/Li.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Li = ({ data }) => {
+  return <li className="m-0">{data}</li>;
+};
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,36 @@
+import { useEffect, useState } from "react";
 import profile from "../../assets/img/profile.png";
+import { IoMdMenu } from "react-icons/io";
+import { RxCross2 } from "react-icons/rx";
+import { Li } from "./Li";
+import axios from "axios";
 
 export const Navbar = () => {
+  const [toggle, settoggle] = useState(false);
+  const [navdata, setnavdata] = useState([]);
+  useEffect(() => {
+    const fetch = async () => {
+      const data = await axios.get("navdata.json");
+      setnavdata(data.data);
+    };
+    fetch();
+  }, []);
+  console.log(navdata);
   return (
-    <div className="flex py-5 border-b justify-between items-center">
-      <h1 className="font-bold text-4xl">Knowlege Cafe</h1>
+    <div className="flex py-5 border-b justify-between items-center ">
+      <h1 className="font-bold text-lg lg:text-4xl flex items-center">
+        {toggle ? (
+          <RxCross2 className="md:hidden" onClick={() => settoggle(!toggle)} />
+        ) : (
+          <IoMdMenu className="md:hidden" onClick={() => settoggle(!toggle)} />
+        )}
+        Knowlege Cafe
+      </h1>
+      <ul
+        className={`flex flex-col md:flex-row space-x-4 ${
+          toggle ? "duration-1000 top-20" : "-top-20"
+        }  absolute md:static`}
+      ></ul>
       <img src={profile} alt="" />
     </div>
   );
