a135f54b3ff4f8394bf89917f03c849c46760bce
rjspyk5 <rjspyk5@gmail.com>
2024-03-19 16:54:58 +0600

    bookmark section formatting complete

--- src/Components/Main/BookMark/BookMark.jsx
+++ src/Components/Main/BookMark/BookMark.jsx
@@ -1,11 +1,15 @@
 import React from "react";
-
+import { EachBookmark } from "./EachBookmark";
 export const BookMark = () => {
   return (
     <div>
-      <div>
+      <div className="space-y-6">
         <div className="bg-[#6047EC1A] border border-[#6047EC] py-4">
           <h1 className="text-[#6047EC] text-center">Spend Time On read:</h1>
+        </div>
+        <div className="bg-[#1111110D] p-5 space-y-4">
+          <h1>Bookmarked Blogs : 8</h1>
+          <EachBookmark></EachBookmark>
         </div>
       </div>
     </div>
---
+++ src/Components/Main/BookMark/EachBookmark.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const EachBookmark = () => {
+  return <div className="bg-white p-3 rounded-md">EachBookmark</div>;
+};
