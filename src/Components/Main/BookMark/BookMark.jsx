3a37ac5d41ee261494bdbd84323c4e278ee5eb0b
rjspyk5 <rjspyk5@gmail.com>
2024-03-20 15:17:32 +0600

    dynamic reading time and bookmark length added

--- src/Components/Main/BookMark/BookMark.jsx
+++ src/Components/Main/BookMark/BookMark.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import { EachBookmark } from "./EachBookmark";
 export const BookMark = ({ read }) => {
+  const total = read.reduce(
+    (acc, cur) => parseInt(cur[1].split(" ")[0]) + acc,
+    0
+  );
+
   return (
     <div>
       <div className="space-y-6">
         <div className="bg-[#6047EC1A] border border-[#6047EC] py-4">
-          <h1 className="text-[#6047EC] text-center">Spend Time On read:</h1>
+          <h1 className="text-[#6047EC] text-center">
+            Spend Time On read: {total} minute
+          </h1>
         </div>
         <div className="bg-[#1111110D] p-5 space-y-4">
-          <h1>Bookmarked Blogs : 8</h1>
+          <h1>Bookmarked Blogs : {read.length}</h1>
           {read.map((el, iDx) => (
             <EachBookmark title={el[0]} key={iDx} />
           ))}
