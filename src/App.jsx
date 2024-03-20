1de9fe89e2afacd93ea5476be13c704beaee70bb
rjspyk5 <rjspyk5@gmail.com>
2024-03-20 16:11:02 +0600

    solve loader related issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { Outlet } from "react-router-dom";
-import { Main } from "./Components/Main/Main";
 import { Navbar } from "./Components/Navbar/Navbar";
 
 function App() {
--- src/main.jsx
+++ src/main.jsx
@@ -1,29 +1,42 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import "./index.css";
-
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { About } from "./Components/Navbar/About.jsx";
-import { Main } from "./Components/Main/Main.jsx";
+import App from "./App";
+import { Main } from "./Components/Main/Main";
+import { About } from "./Components/Navbar/About";
+import { Services } from "./Components/Navbar/Services";
+import { Contact } from "./Components/Navbar/Contact";
+import { Portfolio } from "./Components/Navbar/Portfolio";
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     children: [
       {
-        path: "/about",
-        element: <About />,
-      },
-      {
         path: "/",
         element: <Main />,
         loader: () => fetch("data.json"),
       },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/services",
+        element: <Services />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/portfolio",
+        element: <Portfolio />,
+      },
     ],
   },
 ]);
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
