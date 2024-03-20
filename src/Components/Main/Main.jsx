948c06e749ceea2abfb3312e8b266eff818fcba6
rjspyk5 <rjspyk5@gmail.com>
2024-03-20 15:33:35 +0600

    deleted toast functionality

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -2,20 +2,29 @@ import { useState } from "react";
 import { Blogs } from "./Blogs/Blogs";
 import { BookMark } from "./BookMark/BookMark";
 import { useLoaderData } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 export const Main = () => {
   const data = useLoaderData();
   const [read, setread] = useState([]);
   const handleMarkAsRead = (ob) => {
     setread([...read, ob]);
   };
+  const notify = () =>
+    toast.error("Already read", {
+      position: "top-center",
+    });
   return (
-    <main className="grid grid-cols-1 lg:grid-cols-3 gap-6">
-      <div className="col-span-2 order-2 lg:order-1">
-        <Blogs data={data} handleMarkAsRead={handleMarkAsRead}></Blogs>
-      </div>
-      <div className="order-1 lg:order-2">
-        <BookMark read={read}></BookMark>
-      </div>
-    </main>
+    <>
+      <ToastContainer></ToastContainer>
+      <main className="grid grid-cols-1 lg:grid-cols-3 gap-6">
+        <div className="col-span-2 order-2 lg:order-1">
+          <Blogs data={data} handleMarkAsRead={handleMarkAsRead}></Blogs>
+        </div>
+        <div className="order-1 lg:order-2">
+          <BookMark read={read}></BookMark>
+        </div>
+      </main>
+    </>
   );
 };
